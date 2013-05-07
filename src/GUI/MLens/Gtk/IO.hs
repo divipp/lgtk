85812e91b73a90d55e8c8bf9b4ea71a47aff8df6
divipp <divipp@gmail.com>
2013-05-07 14:54:32 +0200

    add a Gtk optimization but turn off by default for now
    Ignore-this: a8d4835f0b13286e22e82d411144e35e
    
    darcs-hash:20130507125432-a99d3-6994aa334316315683e9f85d8cd771723d4f0821

--- src/GUI/MLens/Gtk/IO.hs
+++ src/GUI/MLens/Gtk/IO.hs
@@ -13,6 +13,7 @@ import Control.Monad
 import Control.Monad.Writer
 import Control.Concurrent
 import Data.Maybe
+import Data.IORef
 import Prelude hiding ((.), id)
 
 import Graphics.UI.Gtk hiding (Widget)
@@ -21,7 +22,7 @@ import qualified Graphics.UI.Gtk as Gtk
 import Control.Monad.Restricted
 import GUI.MLens.Gtk.Interface
 
-gtkContext :: (Morph IO IO -> IO Gtk.Widget) -> IO ()
+gtkContext :: (Morph IO IO -> IO SWidget) -> IO ()
 gtkContext m = do
     _ <- unsafeInitGUIForThreadedRTS
     tid <- myThreadId
@@ -31,10 +32,12 @@ gtkContext m = do
             if tid' == tid then e else postGUISync e
     c <- m post
     window <- windowNew
-    set window [ containerBorderWidth := 10, containerChild := c ]
+    set window [ containerBorderWidth := 10, containerChild := snd c ]
     _ <- window `on` deleteEvent $ liftIO ( mainQuit) >> return False
     widgetShowAll window
     mainGUI
+
+type SWidget = (IO (), Gtk.Widget)
 
 -- | Run an @IO@ parametrized interface description with Gtk backend
 runWidget
@@ -44,7 +47,7 @@ runWidget
     -> Morph IO IO
     -> Morph n IO
     -> Widget n m
-    -> m Gtk.Widget
+    -> m SWidget
 runWidget liftInn post' post dca = toWidget
  where
     liftIO' :: MonadIO n => IO a -> n a
@@ -79,39 +82,50 @@ runWidget liftInn post' post dca = toWidget
             s $ \re -> void' $ liftIO' $ on w changed $ fmap (max 0) (comboBoxGetActive w) >>= dca . re
             return' w
         List o xs -> do
+            ws <- mapM toWidget xs
             w <- liftInn $ liftIO' $ case o of
                 Vertical -> fmap castToBox $ vBoxNew False 1
                 Horizontal -> fmap castToBox $ hBoxNew False 1
-            flip mapM_ xs $ toWidget >=> liftInn . liftIO' . containerAdd'' w
-            return' w
+            shs <- forM ws $ liftInn . liftIO' . containerAdd'' w . snd
+            liftM (mapFst (sequence_ shs >>)) $ return'' ws w
         Notebook' s xs -> do
+            ws <- mapM (toWidget . snd) xs
             w <- liftInn $ liftIO' notebookNew
-            forM_ xs $ \(s, i) -> do
-                ww <- toWidget i
-                liftInn . liftIO' . flip (notebookAppendPage w) s $ ww
+            forM_ (zip ws xs) $ \(ww, (s, _)) -> do
+                liftInn . liftIO' . flip (notebookAppendPage w) s $ snd $ ww
             s $ \re -> void' $ liftIO' $ on w switchPage $ dca . re
-            return' w
-        Cell' f -> do
-            w <- liftInn $ liftIO' $ alignmentNew 0 0 1 1
---            w <- lift $ hBoxNew False 1
-            f (unsafeC . toWidget) $ \x -> liftIO' $ do
---                containerForeach w $ widgetHideAll
-                containerForeach w $ containerRemove w
-                containerAdd w x
-                post' $ post $ widgetShowAll x
-            return' w
---                ch <- containerGetChildren w
+            return'' ws w
 
-return' :: Monad m => GObjectClass x => x -> m Gtk.Widget
-return' = return . castToWidget
+        Cell' f -> do
+            let b = False
+            w <- liftInn $ liftIO' $ case b of
+                True -> fmap castToBox $ hBoxNew False 1
+                False -> fmap castToBox $ alignmentNew 0 0 1 1
+            sh <- liftInn $ liftIO $ newIORef $ return ()
+            f (unsafeC . toWidget) $ \x -> liftIO' $ do
+                writeIORef sh $ fst x
+                post' $ post $ fst x
+                containerForeach w $ if b then widgetHideAll else containerRemove w 
+                ch <- containerGetChildren w
+                when (snd x `notElem` ch) $ containerAdd w $ snd x
+            liftM (mapFst (join (readIORef sh) >>)) $ return'' [] w
+
+return' :: Monad m => WidgetClass x => x -> m SWidget
+return' w = return (widgetShowAll w, castToWidget w)
+
+return'' :: Monad m => WidgetClass x => [SWidget] -> x -> m SWidget
+return'' ws w = return (mapM_ fst ws >> widgetShow w, castToWidget w)
 
 void' :: Monad m => m a -> m ()
 void' m = m >> return ()
+
+mapFst f (a, b) = (f a, b)
 
 containerAdd'' w x = do
     a <- alignmentNew 0 0 0 0
     containerAdd a x
     containerAdd w a
     set w [ boxChildPacking a := PackNatural ]
+    return $ widgetShow a
 
 
