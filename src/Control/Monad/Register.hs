7ae4ebbf66c3e578202b9ddba47db62867f1ee06
divipp <divipp@gmail.com>
2013-05-08 08:52:52 +0200

    add symmetric definitions
    Ignore-this: 8381bc137e99b0345c791960e210efa4
    
    darcs-hash:20130508065252-a99d3-833398c7dbc7bff91e8d37cbed7e205d13d93a90

--- src/Control/Monad/Register.hs
+++ src/Control/Monad/Register.hs
@@ -5,10 +5,12 @@ module Control.Monad.Register
     ( Send
     , mapSend
     , voidSend
+    , constSend
     , Receive
+    , mapReceive
+    , voidReceive
     , IC (..)
     , MonadRegister (..)
-    , constSend
     , rEffect
     ) where
 
@@ -17,7 +19,7 @@ import Control.Monad.Restricted
 type Send m a = (a -> EffectM m ()) -> m ()
 
 mapSend :: (a -> b) -> Send m a -> Send m b
-mapSend f g h = g $ \a -> h $ f a
+mapSend f = (. (. f))
 
 {- not possible, @Send m@ is not an applicative functor
 lift2 :: (a -> b -> c) -> Send m a -> Send m b -> Send m c
@@ -27,7 +29,16 @@ lift2 f ga gb h = ga $ \a -> gb $ \b -> h $ f a b
 voidSend :: Monad m => Send m a
 voidSend _ = return ()
 
+constSend :: (MonadRegister m) => a -> Send m a 
+constSend a f = liftEffectM $ f a
+
 type Receive m a = ((a -> EffectM m ()) -> EffectM m ()) -> m ()
+
+mapReceive :: (b -> a) -> Receive m a -> Receive m b
+mapReceive f = (. (. (. f)))
+
+voidReceive :: Monad m => Receive m a
+voidReceive _ = return ()
 
 data IC m a = forall b . Eq b => IC (R (PureM m) b) (b -> C m a)
 
@@ -41,9 +52,6 @@ class (Monad m, Monad (PureM m), Monad (EffectM m)) => MonadRegister m where
     toSend :: Bool -> IC m a -> Send m a
 
     toReceive :: Eq a => (a -> PureM m ()) -> Receive m a
-
-constSend :: (MonadRegister m) => a -> Send m a 
-constSend a f = liftEffectM $ f a
 
 rEffect :: (MonadRegister m, Eq a) => R (PureM m) a -> Send m a
 rEffect r = toSend False (IC r return)
