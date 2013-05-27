2b32070526448fc1e1573cc440f6e1922f350993
divipp <divipp@gmail.com>
2013-05-27 19:42:29 +0200

    refactoring
    Ignore-this: 81d02f84da82dc223441f7da1b158694
    
    darcs-hash:20130527174229-a99d3-f049e4082f2dd60750b16baa73503102864697f7

--- src/LGtk/Demos/IntListEditor.hs
+++ src/LGtk/Demos/IntListEditor.hs
@@ -40,7 +40,7 @@ intListEditor def maxi list_ range = action $ do
                 , smartButton (return "-1 Sel")     list $ map $ mapSel (+(-1))
                 ]
             , label $ liftM (("Sum: " ++) . show . sum . map fst) sel
-            , action $ listEditor def itemEditor list_
+            , action $ listEditor def (map itemEditor [0..]) list_
             ]
         , (,) "Settings" $ hcat
             [ label $ return "Create range"
@@ -54,29 +54,28 @@ intListEditor def maxi list_ range = action $ do
         [ label $ return $ show (i+1) ++ "."
         , entryShow $ fstLens `lensMap` r
         , checkbox $ sndLens `lensMap` r
-        , button_ (return "Del")  (return True) $ modRef list_ $ \xs -> take i xs ++ drop (i+1) xs
-        , button_ (return "Copy") (return True) $ modRef list_ $ \xs -> take (i+1) xs ++ drop i xs
+        , button_ (return "Del")  (return True) $ modRef list $ \xs -> take i xs ++ drop (i+1) xs
+        , button_ (return "Copy") (return True) $ modRef list $ \xs -> take (i+1) xs ++ drop i xs
         ]
 
     safeList = lens id (const . take maxi) `lensMap` list
 
-    sel = liftM (filter snd) $ readRef list_
+    sel = liftM (filter snd) $ readRef list
 
-    len = joinRef $ liftM ((`lensMap` eqRef (toRef safeList)) . lens length . extendList) $ readRef range
+    len = joinRef $ liftM ((`lensMap` safeList) . lens length . extendList) $ readRef range
     extendList r n xs = take n $ (reverse . drop 1 . reverse) xs ++
         (uncurry zip . (iterate (+ if r then 1 else 0) *** repeat)) (head $ reverse xs ++ [def])
 
     mapSel f (x, y) = (if y then f x else x, y)
 
-listEditor :: EffRef m => a -> (Int -> Ref m a -> m (Widget m)) -> Ref m [a] -> m (Widget m)
-listEditor def ed = editor 0 where
-  editor i r = do
+listEditor :: EffRef m => a -> [Ref m a -> m (Widget m)] -> Ref m [a] -> m (Widget m)
+listEditor def (ed: eds) r = do
     q <- extRef r listLens (False, (def, []))
     return $ cell (liftM fst $ readRef q) $ \b -> case b of
         False -> empty
         True -> action $ do
-            t1 <- ed i $ fstLens . sndLens `lensMap` q
-            t2 <- editor (i+1) $ sndLens . sndLens `lensMap` q
+            t1 <- ed $ fstLens . sndLens `lensMap` q
+            t2 <- listEditor def eds $ sndLens . sndLens `lensMap` q
             return $ vcat [t1, t2]
 
 
